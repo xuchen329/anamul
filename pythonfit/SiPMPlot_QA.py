6429568326e562e879fded1559c0bd39bda8aab0
xuchen <chen.xu@desy.de>
2013-07-03 20:40:56 +0200

    bug fix SiPMPlot_QA.py

--- pythonfit/SiPMPlot_QA.py
+++ pythonfit/SiPMPlot_QA.py
@@ -109,7 +109,7 @@ def GainvsVolt(folderpath):
         plt.savefig(pp,format='pdf')
         pp.close()
 
-        txtfilename=folderpath+'/../'+str(matrixnr)+"_Gain.txt"
+        txtfilename=folderpath+'/../M'+str(matrixnr)+"_Gain.txt"
         with open(txtfilename,"a") as f:
             f.write(channel)
             f.write("\t")
